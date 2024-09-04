d22b4be93476d95af87a8cb62a3b232f532eb81a
sszyc8 <sszyc8@qq.com>
2024-09-04 13:56:51 +0800

    test

--- routes/[...].ts
+++ routes/[...].ts
@@ -1,4 +1,5 @@
-
+export default defineEventHandler(() => {
+  return `
 <h1>Hello Vben Admin</h1>
 <h2>Mock service is starting</h2>
 <ul>
@@ -7,3 +8,5 @@
 <li><a href="/api/auth/codes">/api/auth/codes</a></li>
 <li><a href="/api/auth/login">/api/auth/login</a></li>
 </ul>
+`;
+});
